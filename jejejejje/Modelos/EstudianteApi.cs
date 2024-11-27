8528de9763a1bf0d832854c5f7f8e1c9a1a92a07
ptrickill <daniel.diaz.mosquera8@gmail.com>
2024-11-27 08:09:31 -0500

    12 commit

---
+++ jejejejje/Modelos/EstudianteApi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jejejejje.Modelos
+{
+    public class EstudianteApi
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public Address Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public List<string> Courses { get; set; }
+        public double Gpa { get; set; }
+        public string Image { get; set; }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ jejejejje/Repositories/EstudianteUdlaPorAPIsRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using jejejejje.Interfaces;
+using jejejejje.Modelos;
+using Newtonsoft.Json;
+
+namespace jejejejje.Repositories
+{
+    public class EstudianteUdlaPorAPIsRepository : IEstudianteUdlaRepository
+    {
+        public string _urlEndpoint
+        public bool ActualizarEstudianteUdla(EstudianteUdla estudiante)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CrearEstudianteUdla(EstudianteUdla estudiante)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EstudianteUdla DevuelveEstudiantesUdla(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EstudianteUdla> DevuelveListaEstudiantes()
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var response = httpClient.GetAsync(_urlEndpoint).Result;
+                var json_data = response.Content.ReadAsStringAsync().Result;
+
+                List<EstudianteApi> estudianteAPI = JsonConvert.DeserializeObject<List<EstudianteApi>>(json_data);
+            }
+        }
+
+        public bool EliminarEstudianteUdla(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
